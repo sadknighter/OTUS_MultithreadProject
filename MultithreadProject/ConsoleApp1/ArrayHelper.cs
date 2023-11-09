4757b777949bede5dd5835703a9b2e57aed75346
bigrunner <sadknighter@gmail.com>
2023-11-09 07:55:08 +0300

    feat(methods): added calculations of int array

---
+++ MultithreadProject/ConsoleApp1/ArrayHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1
+{
+    public static class ArrayHelper
+    {
+
+        public static long SimpleFinderSum(int[] digits)
+        {
+            var sum = 0;
+            foreach (var item in digits)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static long ThreadListFinderSum(int[] digits) 
+        {
+            int sumPartsCount = 5;
+            var digitsLength = digits.Length;
+            var peacesArrayCount = digitsLength / sumPartsCount;
+            long[] partialSums = new long[sumPartsCount];
+
+            Parallel.For(0, sumPartsCount, (counter) =>
+            {
+                int sum = 0;
+                for (int i = counter * peacesArrayCount; i < (counter + 1) * peacesArrayCount; i++)
+                {
+                    if (i == digitsLength + 1)
+                    {
+                        break;
+                    }
+
+                    sum += digits[i];
+                }
+
+                partialSums[counter] = sum;
+            });
+
+            return partialSums.Sum();
+        }
+
+        public static long LinqFinderSum(int[] digits)
+        {
+            return digits.Sum();
+        }
+
+        public static long ParallelLinqFinderSum(int[] digits)
+        {
+            return digits.AsParallel().Sum();
+        }
+
+    }
+}
---
+++ MultithreadProject/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+var attempts = new List<int>
+{
+    10, 100, 10000, 100000, 1000000, 10000000
+};
+
+foreach (var attempt in attempts)
+{
+    Console.WriteLine($"Attempt with number of ints in array: {attempt}");
+    var numOfInts = attempt;
+    Random rand = new Random();
+    var numbers = Enumerable.Range(0, 10000)
+                                 .Select(i => new Tuple<int, int>(rand.Next(numOfInts), i))
+                                 .OrderBy(i => i.Item1)
+                                 .Select(i => i.Item2).ToArray();
+
+    Stopwatch sw = Stopwatch.StartNew();
+    var serialSum = ArrayHelper.SimpleFinderSum(numbers);
+    sw.Stop();
+    Console.WriteLine($"Method=SimpleSumFind\tSum: \t{serialSum:n0} \t Time: {sw.ElapsedTicks:n0}\t ticks");
+
+    sw = Stopwatch.StartNew();
+    var threadListSum = ArrayHelper.ThreadListFinderSum(numbers);
+    sw.Stop();
+    Console.WriteLine($"Method=ThreadSumFind\tSum: \t{threadListSum:n0} \t Time: {sw.ElapsedTicks:n0}\t ticks");
+
+    sw = Stopwatch.StartNew();
+    var linqSum = ArrayHelper.LinqFinderSum(numbers);
+    sw.Stop();
+    Console.WriteLine($"Method=LinqSumFind\tSum: \t{threadListSum:n0} \t Time: {sw.ElapsedTicks:n0}\t ticks");
+
+    sw = Stopwatch.StartNew();
+    var parallelLinqSum = ArrayHelper.ParallelLinqFinderSum(numbers);
+    sw.Stop();
+    Console.WriteLine($"Method=ParallelLinq\tSum: \t{threadListSum:n0} \t Time: {sw.ElapsedTicks:n0}\t ticks");
+
+    Console.WriteLine("End of Attempt");
+    Console.WriteLine("");
+}
+
